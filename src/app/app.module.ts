8a773e9319234492007155349c89c251ab6d85d1
jamalcubo <jamal.cubo@gmail.com>
2019-05-07 03:51:44 -0300

    1 file modified, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HomeComponent } from './home/home.component';
-import { ContatoComponent } from './contato/contato.component';
-import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, ContatoComponent, QuemSomosComponent ],
+  declarations: [ AppComponent, HelloComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
