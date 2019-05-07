5a519814023ce2de621b41484f15e42fbe9daf96
jamalcubo <jamal.cubo@gmail.com>
2019-05-07 03:44:08 -0300

    Criando os componentes contato, home e quem somos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, ContatoComponent, QuemSomosComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
