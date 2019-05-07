5d3580793c5b3d2eddc7f67b33fbee2a7fd49892
jamalcubo <jamal.cubo@gmail.com>
2019-05-07 03:39:41 -0300

    incluindo a biblioteca moment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import moment from 'moment';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  startDate: moment.Moment = moment('1/1/2017');
   name = 'Angular';
+  currentDate: moment.Moment = moment();
+  currentTime: string = moment().format('M/D/YYYY hh:mm:ss a');
+  daysFrom2017: number;
+  humanized: string;
+  humanizedNow: string;
+  weeks: number;
+
+  constructor() {
+    this.humanized = moment.duration(moment().diff(this.startDate)).humanize();
+    this.humanizedNow = moment.duration(moment().diff(moment())).humanize();
+
+    // if you need to force to number of days
+    this.daysFrom2017 = this.currentDate.diff(moment('1/1/2017'), 'days');
+
+    // if you need to force to number of weeks
+    this.weeks = moment().diff(this.startDate, 'week');
+    
+    console.log('Days from 1/1/2017:', this.daysFrom2017);
+    console.log('Current Time:', this.currentTime);
+  }
 }
+
